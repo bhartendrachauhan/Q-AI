86df0d11d9752472aee7881c7afd21bc4520081e
Bhartendra Chauhan <bhartendrachauhan@gmail.com>
2025-04-05 10:38:19 +0530

    changes

--- src/utils/base_url.js
+++ src/utils/base_url.js
@@ -1,3 +1,4 @@
 // export const base_url = "https://q-ai-backend-ti5j.onrender.com"
-//  export const base_url = "http://localhost:3000"
- export const base_url = "https://q-ai-backend-main.onrender.com"
+//  export const base_url = "http://localhost:3001"
+//  export const base_url = "https://q-ai-backend-main.onrender.com"
+ export const base_url = "https://q-ai-backend-iggl.onrender.com"
